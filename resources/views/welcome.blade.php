c94216e7028e3416819e20cafb17696c8e521e11
relliv <epsilonelittr@gmail.com>
2024-06-04 19:26:52 +0300

    Add page title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>
+        License Server Example
+    </title>
 
-    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     <style>
@@ -33,6 +34,12 @@
 </head>
 
 <body>
+    <h1>
+        Licenses
+    </h1>
+
+    <hr>
+
     <table>
         <thead>
             <tr>
